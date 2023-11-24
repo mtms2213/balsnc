34b038d710739f9b89bbe796229f49154487aff5
mtms2213 <151932230+mtms2213@users.noreply.github.com>
2023-11-24 19:01:55 +0530

    Add files via upload

---
+++ DESExample.java
@@ -0,0 +1,54 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Scanner;
+
+class DESExample {
+
+    Cipher ecipher;
+    Cipher dcipher;
+
+    DESExample(SecretKey key) throws Exception {
+        ecipher = Cipher.getInstance("DES");
+        dcipher = Cipher.getInstance("DES");
+        ecipher.init(Cipher.ENCRYPT_MODE, key);
+        dcipher.init(Cipher.DECRYPT_MODE, key);
+    }
+
+    public String encrypt(String str) throws Exception {
+        // Encode the string into bytes using utf-8
+        byte[] utf8 = str.getBytes("UTF8");
+
+        // Encrypt
+        byte[] enc = ecipher.doFinal(utf8);
+
+        // Encode bytes to base64 to get a string
+
+        return Base64.getEncoder().encodeToString(enc);
+
+    }
+
+    public String decrypt(String str) throws Exception {
+        // Decode base64 to get bytes
+        byte[] dec = Base64.getDecoder().decode(str);
+
+        byte[] utf8 = dcipher.doFinal(dec);
+
+        // Decode using utf-8
+        return new String(utf8, "UTF8");
+    }
+
+    public static void main(String[] argv) throws Exception {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the Plain Text:");
+	String inputStr = sc.nextLine();
+        SecretKey key = KeyGenerator.getInstance("DES").generateKey();
+        DESExample encrypter = new DESExample(key);
+        String encrypted = encrypter.encrypt(inputStr);
+        System.out.println("Encrypted Value: " + encrypted);
+        String decrypted = encrypter.decrypt(encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+    }
+}
